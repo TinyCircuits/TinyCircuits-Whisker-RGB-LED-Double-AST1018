45731ca024c80ce1823956480948a1f9e79fe831
TinyCircuits <info@tinycircuits.com>
2019-07-30 14:53:13 -0400

    updated to use Port 0

--- examples/RGB-LED_Whisker_example/RGB-LED_Whisker_example.ino
+++ examples/RGB-LED_Whisker_example/RGB-LED_Whisker_example.ino
@@ -7,10 +7,8 @@
 #include <Wire.h> 
 #include "Adafruit_NeoPixel.h"
 
-#define PIN            A2
-#define NUMPIXELS      32
-
-const int powerPin = 4;
+#define PIN            A0 // corresponds to Port 0 (A1 = Port1, A2 = Port 2, A3 = Port 3)
+#define NUMPIXELS      1 // this is the number of RGB LEDs connected to the pin
 
 Adafruit_NeoPixel pixels = Adafruit_NeoPixel(NUMPIXELS, PIN, NEO_GRB + NEO_KHZ800);
 
@@ -18,10 +16,7 @@ int delayval = 500; // delay for half a second
 
 void setup() {
   Wire.begin(); 
-  pinMode(powerPin, OUTPUT);
-  digitalWrite(powerPin, HIGH);
 
-  selectPort(2); 
   pixels.begin(); // This initializes the NeoPixel library.
   pixels.show();
 }
@@ -30,13 +25,5 @@ void loop() {
   for (int i = 0; i <= NUMPIXELS; i++) {
     pixels.setPixelColor(i, pixels.Color(0, 150, 0)); // Moderately bright green color.
     pixels.show(); // This sends the updated pixel color to the hardware.
-    SerialUSB.println("Hello");
   }
-
-}
-
-void selectPort(int port) {
-  Wire.beginTransmission(0x70);
-  Wire.write(0x04 + port);
-  Wire.endTransmission();
 }
